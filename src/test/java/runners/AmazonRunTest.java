5495ce8689d36528a9012e7f57c33b00b7dfcca8
Arrones Alcañiz <ma.arrones@ibermatica.com>
2022-09-30 18:28:39 +0200

    pom actualizado

--- src/test/java/runners/AmazonRunTest.java
+++ src/test/java/runners/AmazonRunTest.java
@@ -9,12 +9,12 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
 		plugin = {"pretty"},
-		features="src/test/resources/features/login/RealizarLogin.feature", //ok
+		//features="src/test/resources/features/login/RealizarLogin.feature", //ok
 		//features="src/test/resources/features/login/borrar/Buscar.feature", // FALSE
 		//	features="src/test/resources/features/mal/loginIncorrecto.feature",  FASLE
-       // features="src/test/resources/features/buscar/buscarPorPrecios.feature",
-		//	features="src/test/resources/features/buscar/BuscarProductosMenorCienEuros.feature",
+        features="src/test/resources/features/buscar/buscarPorPrecios.feature",
+		
 glue= "steps"  )
-public class AmazonRunner {
+public class AmazonRunTest {
 
 }
